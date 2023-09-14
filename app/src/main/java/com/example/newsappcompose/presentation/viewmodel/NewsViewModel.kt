0c04acef89365339817703c57a668dc4da66bcbe
Nadine Islam <nadeen.kamaal@gmail.com>
2023-09-14 16:21:51 +0300

    Upgrade gradle

--- app/src/main/java/com/example/newsappcompose/presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsappcompose/presentation/viewmodel/NewsViewModel.kt
@@ -2,16 +2,17 @@ package com.example.newsappcompose.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.newsappcompose.domain.usecase.NewsUseCase
 import com.example.newsappcompose.data.model.NewsResponse
+import com.example.newsappcompose.data.repository.NewsRepository
 import com.example.newsappcompose.data.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
-class NewsViewModel @Inject constructor(private val newsUseCase: NewsUseCase) : ViewModel() {
+class NewsViewModel @Inject constructor(private val newsRepository: NewsRepository) : ViewModel() {
     private val _news: MutableStateFlow<Resource<NewsResponse>> =
         MutableStateFlow(Resource.Loading())
     val news: StateFlow<Resource<NewsResponse>> = _news
@@ -21,7 +22,15 @@ class NewsViewModel @Inject constructor(private val newsUseCase: NewsUseCase) : 
     }
     private fun getNews() = viewModelScope.launch {
         _news.value = Resource.Loading()
-        val response = newsUseCase()
-        _news.value = response
+        val response = newsRepository.getNews("us")
+        _news.value = handleNewsResponse(response)
+    }
+    private fun handleNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
     }
 }
