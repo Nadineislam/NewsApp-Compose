b2b046b4f40d74316916af7c94f95f1f4f514c19
Nadine Islam <nadeen.kamaal@gmail.com>
2023-10-20 15:12:59 +0300

    removing fakeRepository class, instead make an anonymous object called fakeErrorNewsRepository in NewsViewModelTestDataUtil.kt , moving the list of articles in NewsViewModelTestDataUtil.kt and simplify the success test case

--- app/src/test/java/com/example/newsappcompose/news_home_feature/NewsViewModelTest.kt
+++ app/src/test/java/com/example/newsappcompose/news_home_feature/NewsViewModelTest.kt
@@ -4,18 +4,16 @@ import com.example.newsappcompose.base.MainCoroutineExt
 import com.example.newsappcompose.core.utils.Resource
 import com.example.newsappcompose.news_home_feature.data.remote.dto.SourceDto
 import com.example.newsappcompose.news_home_feature.domain.model.Article
-import com.example.newsappcompose.news_home_feature.domain.model.NewsResponse
-import com.example.newsappcompose.news_home_feature.domain.repository.NewsRepository
 import com.example.newsappcompose.news_home_feature.presentation.viewmodel.NewsViewModel
+import com.example.newsappcompose.util.expectedArticle
+import com.example.newsappcompose.util.fakeErrorNewsRepository
+import com.example.newsappcompose.util.fakeSuccessNewsRepository
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import retrofit2.Response
 
 @ExperimentalCoroutinesApi
 class NewsViewModelTest {
@@ -23,50 +21,23 @@ class NewsViewModelTest {
     val mainCoroutineExt = MainCoroutineExt()
     private lateinit var viewModel: NewsViewModel
     private lateinit var fakeGetNews: FakeNewsUseCase
-    private lateinit var fakeNewsRepository: FakeNewsRepository
 
     @Before
     fun setup() {
-        fakeNewsRepository = FakeNewsRepository()
-        fakeGetNews = FakeNewsUseCase(fakeNewsRepository)
+        fakeGetNews = FakeNewsUseCase(fakeSuccessNewsRepository)
         viewModel = NewsViewModel(fakeGetNews)
     }
 
     @Test
-    fun `when getNews is called then articles should be retrieved`() = runTest {
-        val expectedArticle = Article(
-            author = "John Doe",
-            content = "Lorem ipsum dolor sit amet",
-            description = "Lorem ipsum dolor sit amet",
-            publishedAt = "2023-10-15T12:00:00Z",
-            source = SourceDto("1","CNN"),
-            title = "Sample Article",
-            url = "https://www.example.com/article/1",
-            urlToImage = "https://www.example.com/article/1/image.jpg"
-        )
-
-        val actualResource = viewModel.news.value
-        val actualNewsResponse = actualResource.data
-
+    fun `when getNews is called with success state then articles should be retrieved`() = runTest {
         viewModel.getNews()
-
-        val actualArticles = actualNewsResponse!!.articles
-        val actualArticle = actualArticles.first()
-
-        assertEquals(expectedArticle, actualArticle)
+        assertEquals(expectedArticle, viewModel.news.value.data?.articles)
     }
 
     @Test
     fun `when getNews is called then error should be retrieved`() = runTest {
-        val fakeRepository = object : NewsRepository {
-            override suspend fun getNews(countryCode: String): Response<NewsResponse> {
-                return Response.error(
-                    500,
-                    "Error".toResponseBody("application/json".toMediaTypeOrNull())
-                )
-            }
-        }
-        val fakeNewsUseCase = FakeNewsUseCase(fakeRepository)
+
+        val fakeNewsUseCase = FakeNewsUseCase(fakeErrorNewsRepository)
         viewModel = NewsViewModel(fakeNewsUseCase)
 
         viewModel.getNews()
--- app/src/test/java/com/example/newsappcompose/news_home_feature/NewsViewModelTestDataUtil.kt
+++ app/src/test/java/com/example/newsappcompose/news_home_feature/NewsViewModelTestDataUtil.kt
@@ -1,13 +1,34 @@
-package com.example.newsappcompose.news_home_feature
+package com.example.newsappcompose.util
 
 import com.example.newsappcompose.news_home_feature.data.remote.dto.SourceDto
 import com.example.newsappcompose.news_home_feature.domain.model.Article
 import com.example.newsappcompose.news_home_feature.domain.model.NewsResponse
 import com.example.newsappcompose.news_home_feature.domain.repository.NewsRepository
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.Response
 
-class FakeNewsRepository: NewsRepository {
-override suspend fun getNews(countryCode: String): Response<NewsResponse> {
+val expectedArticle =  listOf( Article(
+    author = "John Doe",
+    content = "Lorem ipsum dolor sit amet",
+    description = "Lorem ipsum dolor sit amet",
+    publishedAt = "2023-10-15T12:00:00Z",
+    source = SourceDto("1","CNN"),
+    title = "Sample Article",
+    url = "https://www.example.com/article/1",
+    urlToImage = "https://www.example.com/article/1/image.jpg"
+))
+
+val fakeErrorNewsRepository = object : NewsRepository {
+    override suspend fun getNews(countryCode: String): Response<NewsResponse> {
+        return Response.error(
+            500,
+            "Error".toResponseBody("application/json".toMediaTypeOrNull())
+        )
+    }
+}
+val fakeSuccessNewsRepository=object :NewsRepository{
+    override suspend fun getNews(countryCode: String): Response<NewsResponse> {
         val articles = listOf(
             Article(
                 author = "John Doe",
@@ -24,5 +45,5 @@ override suspend fun getNews(countryCode: String): Response<NewsResponse> {
         val fakeNewsResponse = NewsResponse(articles, "ok", articles.size)
         return Response.success(fakeNewsResponse)
 
-}
+    }
 }
